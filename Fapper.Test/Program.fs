79da0f802819ecffdfaa2875c0ff8e601a2c8665
Ville M. Vainio <ville.vainio@basware.com>
2018-01-29 19:44:44 +0200

    actually some asserts in tests

--- Fapper.Test/Program.fs
+++ Fapper.Test/Program.fs
@@ -4,6 +4,12 @@ open System
 open Fapper
 open TrivialTestRunner
 
+let rendersTo expected (frags: Frag list)  = 
+    let rendered = frags |> serializeSql SqlSyntax.Any
+    let ok = rendered = expected
+    if (not ok) then do 
+        printfn "Got\n%s\ninstead" (rendered.Replace("\n", "\\n"))
+    Assert.IsTrue(ok)
 
 type Tests() =
     [<Case>]
@@ -19,8 +25,9 @@ type Tests() =
             ]
             WhereS "foo > bar"
         ]
+        upd |> rendersTo "update employee\nset salary = 10, Name = 'heimo'\nwhere foo > bar"
 
-        upd |> (serializeSql SqlSyntax.Any) |> printfn "%s"
+        //upd |> (serializeSql SqlSyntax.Any) |> printVerbatim
 
         let query = [
             Select <| Emp.Cols ["id";"name"; "salary"; "team"]
@@ -38,7 +45,7 @@ type Tests() =
             GroupBy ["team"]
             OrderBy ["salary"]
         ]
-        query |> serializeSql SqlSyntax.Any |> printfn "%s"
+        query |> rendersTo "select employee.id, employee.name, employee.salary, employee.team\nselect employee.Foo as testalias\nfrom employee\nwhere salary > 1000\n\nwhere foo > bar\n\ninner join organization EMPLOYEE on employee.OrgID=EMPLOYEE.ID\nwhere employee.Company=organization.Id\ngroup by team\norder by salary"
 
         let nested = [
             SelectS ["*"]
@@ -48,15 +55,15 @@ type Tests() =
                             From User
             ])
         ]
+        nested |> rendersTo "select *\nfrom\n(\n    select *\n    from USER_DATA\n) root"
         let out2 = serializeSql SqlSyntax.Any nested
 
         let values = [ "a","1" ; "b","2"]
         let writes = [
                         Insert(Emp, values)
                      ] 
-
+        writes |> rendersTo "insert into employee (a,b) values (1,2)"
     
-        writes |> serializeSql SqlSyntax.Any |> printfn "%s"
         ()
 
 [<EntryPoint>]
