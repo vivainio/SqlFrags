f9a3037eed28e9df9310a0b2736c5e93b03981e6
Ville M. Vainio <ville.vainio@basware.com>
2018-01-29 19:16:17 +0200

    Use trivialtestrunner for test

--- Fapper.Test/Program.fs
+++ Fapper.Test/Program.fs
@@ -2,59 +2,67 @@
 
 open System
 open Fapper
+open TrivialTestRunner
 
-let testOrm() =
-    let Emp = Table "employee"
-    let Org = Table "organization"
-    let User = Table "USER_DATA"
-    let upd = [
-        Update Emp
-        Set [
-            "salary", "10"
-            "Name", "'heimo'"
-        ]
-        WhereS "foo > bar"
-    ]
 
-    upd |> (serializeSql SqlSyntax.Any) |> printfn "%s"
-
-    let query = [
-        Select <| Emp.Cols ["id";"name"; "salary"; "team"]
-        SelectAs [Emp?Foo, "testalias"]
-        From Emp
-        WhereS "salary > 1000"
-        Many [
-            Skip
+type Tests() =
+    [<Case>]
+    static member TestOrm() =
+        let Emp = Table "employee"
+        let Org = Table "organization"
+        let User = Table "USER_DATA"
+        let upd = [
+            Update Emp
+            Set [
+                "salary", "10"
+                "Name", "'heimo'"
+            ]
             WhereS "foo > bar"
-
-            Skip
         ]
-        JoinOn( Org.Col "ID", Emp.Col "OrgID", Table "EMPLOYEE", "")
-        Where [Emp?Company == Org?Id]
-        GroupBy ["team"]
-        OrderBy ["salary"]
-    ]
-    query |> serializeSql SqlSyntax.Any |> printfn "%s"
 
-    let nested = [
-        SelectS ["*"]
-        Raw "from"
-        NestAs("root",  [
-                        SelectS ["*"]
-                        From User
-        ])
-    ]
-    let out2 = serializeSql SqlSyntax.Any nested
+        upd |> (serializeSql SqlSyntax.Any) |> printfn "%s"
 
-    let values = [ "a","1" ; "b","2"]
-    let writes = [
-                    Insert(Emp, values)
-                 ] 
+        let query = [
+            Select <| Emp.Cols ["id";"name"; "salary"; "team"]
+            SelectAs [Emp?Foo, "testalias"]
+            From Emp
+            WhereS "salary > 1000"
+            Many [
+                Skip
+                WhereS "foo > bar"
+
+                Skip
+            ]
+            JoinOn( Org.Col "ID", Emp.Col "OrgID", Table "EMPLOYEE", "")
+            Where [Emp?Company == Org?Id]
+            GroupBy ["team"]
+            OrderBy ["salary"]
+        ]
+        query |> serializeSql SqlSyntax.Any |> printfn "%s"
+
+        let nested = [
+            SelectS ["*"]
+            Raw "from"
+            NestAs("root",  [
+                            SelectS ["*"]
+                            From User
+            ])
+        ]
+        let out2 = serializeSql SqlSyntax.Any nested
+
+        let values = [ "a","1" ; "b","2"]
+        let writes = [
+                        Insert(Emp, values)
+                     ] 
 
     
-    writes |> serializeSql SqlSyntax.Any |> printfn "%s"
-    ()
+        writes |> serializeSql SqlSyntax.Any |> printfn "%s"
+        ()
+
 [<EntryPoint>]
 let main argv =
-    testOrm()
+    TRunner.AddTests<Tests>()
+    TRunner.RunTests()
+    TRunner.ReportAll()
+    
     0 // return an integer exit code
